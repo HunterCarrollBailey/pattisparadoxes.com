635265cf97670428a6710fedea7c8c50676b63d3
Hunter Patti <hpatti@pattisparadoxes.net>
2021-11-05 19:04:54 -0700

    Added hubspot code for tracking

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -90,5 +90,9 @@
 
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<!-- Hubspot JS -->
+<!-- Start of HubSpot Embed Code -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/20975247.js"></script>
+<!-- End of HubSpot Embed Code -->
 </body>
 </html>
